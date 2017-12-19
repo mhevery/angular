114da27bb9ba76ecde61a044b6c22e2eddb0e610
Miško Hevery <misko@hevery.com>
2017-12-19 15:01:05 -0800

    build: fix benchmarks for render3

---
+++ packages/core/src/core_render3_private_export.ts
@@ -0,0 +1,28 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+export {
+  renderComponent as ɵrenderComponent,
+  C as ɵC,
+  D as ɵD,
+  E as ɵE,
+  T as ɵT,
+  V as ɵV,
+  b as ɵb,
+  b1 as ɵb1,
+  p as ɵp,
+  c as ɵc,
+  cR as ɵcR,
+  cr as ɵcr,
+  defineComponent as ɵdefineComponent,
+  detectChanges as ɵdetectChanges,
+  e as ɵe,
+  s as ɵs,
+  t as ɵt,
+  v as ɵv,
+} from './render3/index';
